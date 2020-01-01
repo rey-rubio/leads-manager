8672626877e9e0a5453ebf20c5e6e9ca5f0db9b4
rey-rubio <reynerio.r.rubio@gmail.com>
2020-01-01 16:18:38 -0500

    Error handling, alerts, and messages

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import leads from "./leads.js";
+import errors from "./errors.js";
+import messages from "./messages.js";
 
 export default combineReducers({
-  leads
+  leads,
+  errors,
+  messages
 });
